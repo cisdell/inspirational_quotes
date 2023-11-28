e8d90507b61fe9e9b6b4355ffc7aa6e91a126fc3
Andrew Cho <cisdell@gmail.com>
2023-11-28 14:55:59 -0500

    refactored and passed in a function to the components

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -20,19 +20,33 @@ export const fetchQuotes = async (count: number) => {
 };
 
 const Application = () => {
-  const [quote, setQuote] = useState();
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [count, setCount] = useState(10);
 
-  useEffect(() => {
-    fetchRandomQuote().then(setQuote);
-  }, []);
+  // useEffect(() => {
+  //   fetchRandomQuote().then(setQuote);
+  // }, []);
 
-  if (!quote) return <Loading />;
+  console.log(quotes);
+
+  if (!quotes) return <Loading />;
   return (
-    <main className="w-full max-w-2xl py-16 mx-auto">
+    <main className="mx-auto w-full max-w-2xl py-16">
       {/* <InspirationalQuote content={quote.content} source={quote.source} /> */}
-      {/* <Quotes>
-        <div className="grid grid-cols-2 gap-4"></div>
-      </Quotes> */}
+      <Quotes setQuotes={setQuotes}>
+        {/* <div className="grid grid-cols-2 gap-4"> */}
+          {quotes.map((quote) => {
+            return (
+              <InspirationalQuote
+                key={quote.id}
+                content={quote.content}
+                source={quote.source}
+
+              />
+            );
+          })}
+        {/* </div> */}
+      </Quotes>
     </main>
   );
 };
